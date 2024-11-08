1447c8ef66d8d420cfcc81e8ab65ba94737e7916
Zheng.Fabio <fabiozheng927@gmail.com>
2024-11-08 10:39:14 +0100

    aggiunto programmino

---
+++ esercizio.py
@@ -0,0 +1,6 @@
+scelta= 1
+somma = 0
+while scelta ! = 0:
+    scelta= int(input("inserire un numeri (zero per termianre): "))
+    somma = somma + scelta
+    print (f"La somma di tutti i numeri è {somma}")
